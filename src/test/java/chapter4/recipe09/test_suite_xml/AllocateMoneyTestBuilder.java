d7e10922c9140976953fba4d8adfcb486d165a3f
sniksnp <sniksnp@gmail.com>
2014-05-21 19:04:46 +0200

    Use jaxen and dom4j instead of XPathAPI and w3.dom

--- src/test/java/chapter4/recipe09/test_suite_xml/AllocateMoneyTestBuilder.java
+++ src/test/java/chapter4/recipe09/test_suite_xml/AllocateMoneyTestBuilder.java
@@ -1,19 +1,18 @@
 package chapter4.recipe09.test_suite_xml;
 
-import java.io.*;
-import java.text.ParseException;
-import java.util.*;
-
-import javax.xml.parsers.*;
-import javax.xml.transform.TransformerException;
-
 import chapter4.recipe08.data_driven_test_suite.AllocateMoneyTest;
-import com.sun.org.apache.xpath.internal.XPathAPI;
-import junit.framework.Assert;
-
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.jaxen.JaxenException;
+import org.jaxen.dom4j.Dom4jXPath;
 import util.Money;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class AllocateMoneyTestBuilder {
@@ -27,62 +26,50 @@ public class AllocateMoneyTestBuilder {
         return new AllocateMoneyTestBuilder(testFileName).makeTests();
     }
 
-    private List makeTests() throws Exception {
+    @SuppressWarnings("unchecked")
+    public List makeTests() throws Exception {
         List tests = new ArrayList();
-        Document document = makeDocument(testFileName);
-        NodeList testNodes = XPathAPI.selectNodeList(document, "/tests/test");
+        Document document = new SAXReader().read(AllocateMoneyTestBuilder.class.getResourceAsStream(testFileName));
+        List<Element> testNodes = new Dom4jXPath("/tests/test").selectNodes(document);
 
-        for (int i = 0; i < testNodes.getLength(); i++) {
-            Node eachTestNode = testNodes.item(i);
-            AllocateMoneyTest eachAllocateMoneyTest = makeAllocateMoneyTest(eachTestNode);
-            tests.add(eachAllocateMoneyTest);
+        for (Element testNode : testNodes) {
+            tests.add(makeAllocateMoneyTest(testNode));
         }
 
         return tests;
     }
 
-    private AllocateMoneyTest makeAllocateMoneyTest(Node eachTestNode) throws TransformerException, ParseException {
-        Money amountToSplit = parseAsMoney(eachTestNode, "input/amount-to-split");
-        int nWays = parseAsInt(eachTestNode, "input/number-of-ways");
-        NodeList expectedCutNodes = XPathAPI.selectNodeList(eachTestNode, "expected-result/cut");
+    private AllocateMoneyTest makeAllocateMoneyTest(Element testNode) throws JaxenException, ParseException {
+        Money amountToSplit = parseAsMoney(testNode, "input/amount-to-split");
+        int nWays = parseAsInt(testNode, "input/number-of-ways");
+        List<Element> expectedCutNodes = new Dom4jXPath("expected-result/cut").selectNodes(testNode);
         Map expectedCuts = parseExpectedCuts(expectedCutNodes);
         AllocateMoneyTest eachAllocateMoneyTest = new AllocateMoneyTest(amountToSplit, nWays, expectedCuts);
         return eachAllocateMoneyTest;
     }
 
-    private Document makeDocument(String documentFileName) throws FactoryConfigurationError,
-            ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        InputStream testDataAsStream = AllocateMoneyTestBuilder.class.getResourceAsStream(documentFileName);
-        Document document = builder.parse(testDataAsStream);
-        return document;
-    }
-
-    private Map parseExpectedCuts(NodeList expectedCutNodes) throws TransformerException, ParseException {
+    private Map parseExpectedCuts(List<Element> expectedCutNodes) throws JaxenException, ParseException {
         Map expectedCuts = new HashMap();
 
-        for (int i = 0; i < expectedCutNodes.getLength(); i++) {
-            Node eachCutNode = expectedCutNodes.item(i);
-            Money cutAmount = parseAsMoney(eachCutNode, "amount");
-            int numberOfCuts = parseAsInt(eachCutNode, "number");
+        for (Element cutNode : expectedCutNodes) {
+            Money cutAmount = parseAsMoney(cutNode, "amount");
+            int numberOfCuts = parseAsInt(cutNode, "number");
             expectedCuts.put(cutAmount.valueInCents(), numberOfCuts);
         }
 
         return expectedCuts;
     }
 
-    private static int parseAsInt(Node fromNode, String xpathToInt) throws TransformerException {
+    private static int parseAsInt(Element fromNode, String xpathToInt) throws JaxenException {
         String intAsString = getNodeText(fromNode, xpathToInt);
         return Integer.parseInt(intAsString);
     }
 
-    private static String getNodeText(Node fromNode, String xpath) throws TransformerException {
-        Text text = (Text) XPathAPI.selectSingleNode(fromNode, xpath).getFirstChild();
-        return text.getData();
+    private static String getNodeText(Element fromNode, String xpath) throws JaxenException {
+        return String.valueOf(new Dom4jXPath(xpath).selectSingleNode(fromNode));
     }
 
-    private static Money parseAsMoney(Node fromNode, String xpathToMoneyObject) throws TransformerException,
+    private static Money parseAsMoney(Element fromNode, String xpathToMoneyObject) throws JaxenException,
             ParseException {
         String moneyAsString = getNodeText(fromNode, xpathToMoneyObject);
         return new Money(moneyAsString);
