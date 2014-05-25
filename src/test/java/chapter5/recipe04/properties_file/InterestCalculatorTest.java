06a84a88532c676287e856f0d6be5c57c49f8070
sniksnp <sniksnp@gmail.com>
2014-05-25 16:41:46 +0200

    Chapter 5, recipe 4: use a .properties file

---
+++ src/test/java/chapter5/recipe04/properties_file/InterestCalculatorTest.java
@@ -0,0 +1,33 @@
+package chapter5.recipe04.properties_file;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class InterestCalculatorTest {
+    private String propertiesFile = "/interest_rates.properties";
+    private double interestRate, loanAmount;
+    private int loanDuration;
+
+    @Before
+    public void setUp() throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream(propertiesFile);
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        properties.list(System.out);
+
+        interestRate = Double.parseDouble(properties.getProperty("interest.rate"));
+        loanAmount = Double.parseDouble(properties.getProperty("loan.amount"));
+        loanDuration = Integer.parseInt(properties.getProperty("loan.duration.years"));
+    }
+    
+    @Test
+    public void testInterestCalculation() throws Exception {
+        // fake interest calculation
+    }
+
+}
